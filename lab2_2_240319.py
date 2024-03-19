5230730245feec815ddb321516609b2bca22a271
jinnu-dev <jinwoolian@gmail.com>
2024-03-19 16:53:52 +0900

    Update lab2_2_240319.py

--- lab2_2_240319.py
+++ lab2_2_240319.py
@@ -3,4 +3,5 @@ price = int(input("물건값: "))
 outmoney = inmoney-price
 print("거스름돈:", outmoney)
 print("500원 동전의 개수: ", outmoney//500)
-print("100 동전의 개수: ", (outmoney%500)/100)
+print("100 동전의 개수: ", (outmoney%500)//100)
+print("10 동전의 개수: ", ((outmoney%500)%100)//10)
