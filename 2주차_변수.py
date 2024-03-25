27d7656cd195c02c4df3cac966e4960de76d2186
younnnga <163095640+younnnga@users.noreply.github.com>
2024-03-25 12:39:11 +0900

    Create 2주차_변수.py

---
+++ 2주차_변수.py
@@ -0,0 +1,64 @@
+print(100+200)
+
+a=100
+b=200
+c=300
+print(a,'+',b,'=',c)
+
+num1 = 100
+num2 = 50
+result = num1 - num2
+print(num1,"-",num2,'=',result)
+
+str1 = "난생처음"
+str2 = "파이썬"
+result = str1+str2
+print(result)
+
+num1 = 100
+num2 = 200
+result1 = num1 + num2
+result2 = num1 - num2
+result3 = num1 * num2
+result4 = num1 / num2
+print(num1,"+",num2,"=",result1)
+print(num1,"-",num2,"=",result2)
+print(num1,"*",num2,"=",result3)
+print(num1,"/",num2,"=",result4)
+
+num1 = int(input("숫자1==>"))
+num2 = int(input("숫자2==>"))
+result = num1 + num2
+print(result)
+
+username = input("이름:")
+userphone = input("전화번호 :")
+print("제 이름은",username,"이고,연락처는",userphone,"입니다.")
+
+print("택배를 보내기 위한 정보를 입력하세요. ##")
+personName = input("받는 사람:")
+personAddr = input("주소 :")
+weight = int(input("무게(g):"))
+print("** 받는 사람 ==>",personName)
+print('주소 ==>',personAddr)
+print('배송비 ==>',weight*5,"원")
+
+num1 = int(input("숫자1:"))
+num2 = int(input("숫자2:"))
+result1 = num1 + num2
+result2 = num1 - num2
+result3 = num1 * num2
+result4 = num1 / num2
+result5 = num1 % num2
+result6 = num1 ** num2
+print(num1,"+",num2,'=',result1)
+print(num1,"-",num2,'=',result2)
+print(num1,"*",num2,'=',result3)
+print(num1,"/",num2,'=',result4)
+print(num1,"%",num2,'=',result5)
+print(num1,"**",num2,'=',result6)
+
+
+
+
+
