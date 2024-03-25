e0adf48e8248a8012b31809927e44979c849cb38
younnnga <163095640+younnnga@users.noreply.github.com>
2024-03-25 12:39:08 +0900

    Create 3주차_연산자.py

---
+++ 3주차_연산자.py
@@ -0,0 +1,24 @@
+a,b,c = 2,4,6
+print(a/b*c)
+
+q = 5 // 3
+r = 5 % 3
+print(q,r)
+
+num = 5 * 3
+print(num)
+
+num1 = int(input("나눠지는 수:"))
+num2 = int(input("나누는 수 :"))
+q = num1 // num2
+r = num1 % num2
+print(num1,"을",num2,"로 나는 몫은",q,"입니다.")
+print(num1,"을",num2,"로 나는 나머지는",r,"입니다.")
+
+pound = int(input("파운드(1b)를 입력하세요:"))
+kg = pound * 0.453592
+print(pound,"파운드(1b)는",kg,"킬로그램(kb)입니다")
+
+kg = int(input("킬로그램(kg)을 입력하세요 :"))
+pound = kg * 2.204623
+print(kg,"킬로그램(kg)은",pound,"파운드(1b)입니다")
