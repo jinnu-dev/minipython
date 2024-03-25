5583030decc6023c7138305215ce287156fa4ba2
younnnga <163095640+younnnga@users.noreply.github.com>
2024-03-25 12:39:01 +0900

    Create 3주차_연산자2.py

---
+++ 3주차_연산자2.py
@@ -0,0 +1,48 @@
+total = 0
+total -= 900*10
+total -= 3500*5
+total += 1800*2
+total += 4000*4
+total += 1800*5
+total += 2000*4
+total += 1500
+print("오늘 총 매출액은",total,"입니다.")
+
+x = 100
+x += 15
+x -= 10 
+x *= 5
+x /= 2
+x += 3
+x **= 2
+print(x)
+x %= 4
+reminder =int(x)
+print(reminder)
+
+score = int(input("필기 시험점수를 입력하세요:"))
+print(score >= 70)
+
+num1 = 100
+num2 = 200
+print(num1==num2 , num1!=num2)
+
+num = 99
+print(num > 100) and (num < 200)
+print(num == 99) or (num == 100)
+print(not(num==100))
+
+python = 3
+mobile = 2
+excel = 1
+A = 4.5
+B = 3.5
+A0 = 4
+avg = ((python*B)+(mobile*A0)+(excel*A))/(python+mobile+excel)
+print('평균 학점 :',avg)
+
+
+
+
+
+
