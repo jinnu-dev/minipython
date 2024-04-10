a34fc0da2fa5843504889969af7d5224b356f4a9
seul99 <163095876+seul99@users.noreply.github.com>
2024-04-10 19:03:07 +0900

    2주차

--- PythonStudy_04.08.py
+++ PythonStudy_04.08.py
@@ -1,33 +1,11 @@
-# if~elif / for / while
-# if~elif 가위바위보 게임 만들기
-import random
-hand = input("나의 가위/바위/보 ==> ")
-comhand = random.choice(['가위','바위','보'])
+# for / while
+# 500에서 1000까지의 짝수합 출력하기
+i, hap = 0, 0
+for i in range(500,1001,2):
+    hap += i
+print("500에서 1000까지의 짝수의 합 : ", hap)
 
-if hand == "가위":
-    if comhand == "가위" :
-        print("나 : {0}  vs 컴퓨터 : {1}\n결과는 비겼습니다.".format(hand, comhand))
-    elif comhand == "바위" :
-        print("나 : {0}  vs 컴퓨터 : {1}\n결과는 이겼습니다.".format(hand, comhand))
-    elif comhand == "보" :
-        print("나 : {0}  vs 컴퓨터 : {1}\n결과는 졌습니다.".format(hand, comhand))
-elif hand == "바위":
-    if comhand == "가위" :
-        print("나 : {0}  vs 컴퓨터 : {1}\n결과는 졌습니다.".format(hand, comhand))
-    elif comhand == "바위" :
-        print("나 : {0}  vs 컴퓨터 : {1}\n결과는 비겼습니다.".format(hand, comhand))
-    elif comhand == "보" :
-        print("나 : {0}  vs 컴퓨터 : {1}\n결과는 이겼습니다.".format(hand, comhand))
-elif hand == "보":
-    if comhand == "가위" :
-        print("나 : {0}  vs 컴퓨터 : {1}\n결과는 졌습니다.".format(hand, comhand))
-    elif comhand == "바위" :
-        print("나 : {0}  vs 컴퓨터 : {1}\n결과는 이겼습니다.".format(hand, comhand))
-    elif comhand == "보" :
-        print("나 : {0}  vs 컴퓨터 : {1}\n결과는 비겼습니다.".format(hand, comhand))
-
-    
-# 리스트 딕셔너리 튜플
-
-
-# 함수 호출
+i = 0
+while (i<5) :
+    print("즐거운 난생 처음^^")
+    i += 1
